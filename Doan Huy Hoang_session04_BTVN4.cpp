208abd850f22a29533255816cc3ee53dde835d7c
hoangdoan8222-hash <hoangdoan8222@gmail.com>
2025-10-27 22:30:02 +0700

    Add files via upload

---
+++ Doan Huy Hoang_session04_BTVN4.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int thang;
+	printf("Nhap thang (1-12): ");
+	scanf("%d", &thang);
+	switch (thang){
+		case 1:
+		case 3:
+	    case 5:
+	    case 7:
+	    case 8:
+	    case 10:
+	    case 12:
+	    	printf("Thang %d co 31 ngay\n", thang);
+	    	break;
+	    case 4:
+	    case 6:
+	    case 9:
+	    case 11:
+	    	printf("Thang %d co ngay 30 ngay\n", thang);
+	    	break;
+	    case 2:
+	    	printf("Thang 2 co 28 hoac 29 ngay\n");
+	    	break;
+	    default:
+	    	printf("Thang khong hop le\n");
+	    	break;
+	}
+
+   return 0;
+ }
+
