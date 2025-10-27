b71cc580060a926cb7ed34b1b8dee74b703cffae
hoangdoan8222-hash <hoangdoan8222@gmail.com>
2025-10-27 22:27:42 +0700

    Add files via upload

---
+++ Doan Huy Hoang_session04_BTVN1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int number; 
+	scanf("%d" ,&number);
+	if(number > 0){
+		printf("%d la so duong");
+	}else{
+		printf("%d la so am");
+	}
+    
+	   return 0;
+}
+
